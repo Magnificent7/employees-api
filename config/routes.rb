ca26992b74cc39bee12faea72afd44be1f49fb72
dani <dani.zaghian@gmail.com>
2017-06-06 18:41:21 -0700

    namespaced api

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :v1 do
-    get '/employees' => 'employees#index'
-    get '/employees/:id' => 'employees#show'
-    post '/employees' => 'employees#create'
-    patch '/employees/:id' => 'employees#update'
-    delete '/employees/:id' => 'employees#destroy'
-  end
+  namespace :api do
+    namespace :v1 do
+      get '/employees' => 'employees#index'
+      get '/employees/:id' => 'employees#show'
+      post '/employees' => 'employees#create'
+      patch '/employees/:id' => 'employees#update'
+      delete '/employees/:id' => 'employees#destroy'
+    end
 
-  namespace :v2 do
-    get '/employees' => 'employees#index'
-    get '/employees/:id' => 'employees#show'
-    post '/employees' => 'employees#create'
-    patch '/employees/:id' => 'employees#update'
-    delete '/employees/:id' => 'employees#destroy'
+    namespace :v2 do
+      get '/employees' => 'employees#index'
+      get '/employees/:id' => 'employees#show'
+      post '/employees' => 'employees#create'
+      patch '/employees/:id' => 'employees#update'
+      delete '/employees/:id' => 'employees#destroy'
+    end
   end
   
 end
